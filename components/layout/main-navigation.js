733ddf860c2bcfe9cf176e0968d29e04609f234e
m4nkej <mankej91@yahoo.pl>
2023-07-03 11:43:10 +0200

    logout

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,8 +1,14 @@
 import Link from "next/link";
-
+import { useSession, signOut } from "next-auth/react";
 import classes from "./main-navigation.module.css";
 
 function MainNavigation() {
+  const { data: session, status } = useSession();
+  console.log(session, status);
+
+  function logoutHandler() {
+    signOut();
+  }
   return (
     <header className={classes.header}>
       <Link href="/">
@@ -10,15 +16,21 @@ function MainNavigation() {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href="/auth">Login</Link>
-          </li>
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {status === "unauthenticated" && (
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+          )}
+          {status === "authenticated" && (
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          )}
+          {status === "authenticated" && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
